cd6d7d06fd4e1473362814e6d90234c1eb52d9cb
Jason Morgan <jmorgan@f9vs.com>
2015-03-05 09:25:20 -0700

    Have a bidirectional model, the code isn't all in place yet

--- Alerts/AlertsConnector.ps1
+++ Alerts/AlertsConnector.ps1
@@ -17,8 +17,6 @@ Import-Module SMLets
 Import-Module SevOne
 #endregion import modules
 
-
-
 #region Open ServiceManager connection
 ### Maybe something goes here
 #endregion Open ServiceManager connection
@@ -27,54 +25,75 @@ Import-Module SevOne
 Connect-SevOne -ComputerName pdcd18-sevone01a -Credential $SevOneCred
 #endregion Open SevOne Connection
 
-#region Read existing SevOne alerts in SM
-$alerts = Get-SevOneAlert
-$class = Get-SCSMClass -Name Microsoft.SystemCenter.WorkItem.SCOMIncident
-$incidents = Get-SCClassInstance -Class $class
-#Close closed alerts
-#endregion Read existing SevOne alerts in SM
+#region Draw Sources
+$alerts = Get-SevOneAlert | Where-Object {$_.message -notmatch '\[dev\]'}
+$class = Get-SCClass -Name Microsoft.SystemCenter.WorkItem.SCOMIncident # not this class, use the new sevoneclass
+$Inc_Hash = @{}
+$incidents = Get-SCClassInstance -Class $class # add filter to only include open incidents
+$incidents | foreach {$Inc_Hash.Add($_.id,$_)}
+$Incidentstobeclosed = Get-SCClassInstance -Class $class #add filter where stats = resolved and AlertStatus = open
+$Dev_hash =@{}
+$Devices = Get-SevOneDevice | foreach {$Dev_hash.Add($_.id,$_.name)}
+
+#endregion Draw Sources
 
 #region Write alerts to SM
-$alerts = Get-SevOneAlert
-foreach ($a in $alerts)
+$NewAlerts = $alerts.where{$_.id -notin $incidents.SevOneAlertID}
+foreach ($a in $NewAlerts)
 {
   $sev = #Something to set valid
-  $impact = Get-SCSMEnumeration -Name System.WorkItem.TroubleTicket.ImpactEnum.Medium
-  $urgency = Get-SCSMEnumeration -Name System.WorkItem.TroubleTicket.UrgencyEnum.Medium
+  $impact = Get-SCSMEnumeration -Name System.WorkItem.TroubleTicket.ImpactEnum.Medium # might set this on the basis of the object
+  $dev = $Dev_hash.item($a.deviceId)
+  # I think we'll be ok for now just defaulting to medium
+  # Alertnatively we could do it based on alert.message
+  switch ($a.severity)
+    {
+      {$_ -le 3} { $urgency = Get-SCSMEnumeration -Name System.WorkItem.TroubleTicket.UrgencyEnum.High }
+      {$_ -gt 3 -and $_ -le 5} { $urgency = Get-SCSMEnumeration -Name System.WorkItem.TroubleTicket.UrgencyEnum.Medium }
+      {$_ -gt 5} { $urgency = Get-SCSMEnumeration -Name System.WorkItem.TroubleTicket.UrgencyEnum.Low }
+    }
+  
   $props = @{
-      AlertCustomField1 = 25
+      SevOneAlertID = $a.id
       Urgency = $urgency
       Impact = $impact
-      Description = 'Test: Please ignore'
-      Source = 'IncidentSourceEnum.System'
-      Title = 'Test: Please ignore'
+      Description = $a.message
+      Source = 'IncidentSourceEnum.System' # statically set to the new SevOneAlert source once it's available
+      Title = "SevOne: " + $a.message.split('-')[0]
+      SevOneDeviceName = $dev
+      SevOneDeviceId = $a.deviceId
+      SevOneSeverity = $a.severity
+      AlertStatus = 'Open'
     }
+  #create Incident
+  #return IncidentID
 }
-#Tattoo incidents with SevOne AlertID
 #endregion Write alerts to SM
 
+#region Close Incidents with no Alerts
+$incidents = $incidents.where{$_.SevOneAlertid -notin $alerts.id}
+foreach ($i in $incidents)
+  {
+    
+    #Resolve incident
+  }
+#endregion Close Incidents with no Alerts
 
-
+#region Close Alerts for resolved incidents
+foreach ($i in $Incidentstobeclosed)
+  {
+    Close-SevOneAlert -Alert $Inc_Hash.item($i.SevOneAlertID)
+  }
+#endregion Close Alerts for resolved incidents
 <#
-AlertId
-Classification
 
-Description
-DisplayName
-
-Id
-
-LastModifiedSource
-
-MonitoringObjectId
-
-MonitoringRuleId
-
-Priority
-
-Source
-Status
-Title
-Urgency
+[int]AlertId
+[string]DeviceName
+[string]DeviceID
+[int]SevOneSeverity
+[string]SevOneAlertStatus #this should have been a duh moment, SevOne's API doesn't allow us to tatoo the alert
+but that doesn't matter if we add this property.  On creation we set the value to open and we only change it 
+after we have successfully sent a close message to SevOne.  I may actually be a genuis because all of my solutions
+involve someone other than me doing the hard work ;)
 
 #>
